c400deb607c7b3024053ff06061d37d2e59ba745
alievaleila <meliyeva452@gmail.com>
2025-10-27 22:50:45 +0400

    initial commit

--- src/main/java/org/example/enums/bankaccountypes/Main.java
+++ src/main/java/org/example/enums/bankaccountypes/Main.java
@@ -4,14 +4,26 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<BankAccount> accounts = new ArrayList<>();
+        final ArrayList<BankAccount> accounts = new ArrayList<>();
 
-        accounts.add(new BankAccount("Leyla", 500, AccountType.SAVINGS));
-        accounts.add(new BankAccount("Ibrahim", 350, AccountType.FIXED));
-        accounts.add(new BankAccount("Rovshan", 600, AccountType.CHECKING));
+        BankAccount acc=new BankAccount("Leyla", 500, AccountType.SAVINGS);
+        BankAccount acc1=new BankAccount("Ibrahim", 350, AccountType.FIXED);
+        BankAccount acc2=new BankAccount("Rovshan", 600, AccountType.CHECKING);
 
-        for (BankAccount acc : accounts) {
-            acc.showInfo();
+        accounts.add(acc);
+        accounts.add(acc1);
+        accounts.add(acc2);
+
+        acc.deposit(400);
+        acc.withdraw(700);
+        acc1.deposit(1000);
+        acc1.withdraw(100);
+        acc2.deposit(1200);
+
+
+
+        for (BankAccount account : accounts) {
+            account.showInfo();
         }
     }
 }
