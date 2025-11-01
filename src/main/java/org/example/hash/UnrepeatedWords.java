3ba71c8c0ef8c18c6321185c4642a0c8a4d08334
alievaleila <meliyeva452@gmail.com>
2025-11-02 00:33:55 +0400

    initial commit

---
+++ src/main/java/org/example/hash/UnrepeatedWords.java
@@ -0,0 +1,22 @@
+package org.example.hash;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class UnrepeatedWords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the word");
+        String a = sc.nextLine();
+
+        HashSet<Character> letters = new HashSet<>();
+        StringBuilder unique = new StringBuilder();
+        for (int i = 0; i < a.length(); i++) {
+            if (letters.add(a.charAt(i))) {
+                unique.append(a.charAt(i));
+            }
+        }
+        System.out.println(unique.toString());
+    }
+}
+
